829125d3f4e0688eaf4683acd2862056ddde9dc7
hubbergetter <traughber.da@gmail.com>
2026-03-10 14:11:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,24 +16,33 @@ document.addEventListener("DOMContentLoaded", function () {
       if (appName === "color-filters") {
         previewImage.src = "images/color-filters-preview.png";
         launchLink.href = "apps/color-filters/index.html";
+        description.textContent =
+          "Interactive RGB and subtractive filter exploration tool.";
       }
 
       if (appName === "color-mixing") {
         previewImage.src = "images/color-mixing-preview.png";
         launchLink.href = "apps/color-mixing/index.html";
+        description.textContent =
+          "Explore additive (RGB) and subtractive (CMY) color mixing using interactive Venn diagrams.";
       }
 
       if (appName === "graphics-calculator") {
         previewImage.src = "images/graphics-calc-preview.png";
         launchLink.href = "apps/graphics-calculator/index.html";
+        description.textContent =
+          "Interactive graphing calculator for plotting and exploring mathematical functions.";
+      }
+
+      if (appName === "snells-law") {
+        previewImage.src = "images/snells-law-preview.png";
+        launchLink.href = "apps/snells-law/index.html";
+        description.textContent =
+          "Investigate refraction using an interactive Snell's Law simulator. Includes a downloadable lab guide.";
       }
 
     });
   });
 
 });
-
-
-
-
 
